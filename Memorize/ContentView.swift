87ad40e49f4c398a7e32ad60f5e4ccc17f70eec2
Sebastian Pfeufer <sebastianpfeufer@icloud.com>
2022-01-21 12:33:52 +0100

    Created card grid using LazyVGrid

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,14 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    var testTheme = ["🚗", "🚕", "🚙", "🚌", "🚎", "🏎"]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack {
+            LazyVGrid(columns: [GridItem(), GridItem()]) {
+                ForEach(testTheme, id: \.self) { item in
+                    CardView(content: item).aspectRatio(2/3, contentMode: .fit)
+                }
+            }
+        }
+    }
+}
+
+struct CardView: View {
+    var content: String
+    @State var isFaceUp = true
+    
+    var body: some View {
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: 20)
+            if isFaceUp {
+                shape.foregroundColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
+                shape.stroke(lineWidth: 3)
+                Text(content).font(.largeTitle)
+            } else {
+                shape.foregroundColor(.red)
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().preferredColorScheme(.dark)
+        ContentView().preferredColorScheme(.light)
     }
 }
